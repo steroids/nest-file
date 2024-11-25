4f60bfc14bd150b0baddf456bd8381b5224588fc
Danil <danil_ustinov@mail.ru>
2024-11-25 13:49:50 +0700

    remove IDeepPartial from tables

--- src/infrastructure/tables/FileImageTable.ts
+++ src/infrastructure/tables/FileImageTable.ts
@@ -1,6 +1,5 @@
-import {IDeepPartial} from '@steroidsjs/nest/usecases/interfaces/IDeepPartial';
 import {TypeOrmTableFromModel} from '@steroidsjs/nest/infrastructure/decorators/typeorm/TypeOrmTableFromModel';
 import {FileImageModel} from '../../domain/models/FileImageModel';
 
 @TypeOrmTableFromModel(FileImageModel, 'file_image')
-export class FileImageTable implements IDeepPartial<FileImageModel> {}
+export class FileImageTable extends FileImageModel {}
