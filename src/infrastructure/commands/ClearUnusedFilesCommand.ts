3de1f8b1de602b00e6737f9893076c9abf188fa2
Danil <danil_ustinov@mail.ru>
2025-02-14 10:41:22 +0700

    fix: refactoring

--- src/infrastructure/commands/ClearUnusedFilesCommand.ts
+++ src/infrastructure/commands/ClearUnusedFilesCommand.ts
@@ -52,17 +52,17 @@ export class ClearUnusedFilesCommand {
             isDryRun: boolean,
     ) {
         const totalFilesCount = await this.fileService.getCount();
-        const junkFilesIds = await this.fileService.getUnusedFilesIds({
+        const unusedFilesIds = await this.fileService.getUnusedFilesIds({
             ignoredTables: ['file_image'],
             fileNameLike: nameLike,
             isEmpty,
         });
         console.log(`Всего файлов: ${totalFilesCount}`);
-        console.log(`Файлов для удаления: ${junkFilesIds.length}`);
+        console.log(`Файлов для удаления: ${unusedFilesIds.length}`);
 
         if (previewLimit && previewLimit > 0) {
             const exampleFiles = await this.fileService.createQuery()
-                .where(['in', 'id', junkFilesIds.slice(0, previewLimit)])
+                .where(['in', 'id', unusedFilesIds.slice(0, previewLimit)])
                 .many();
             console.table(exampleFiles.map(file => ({
                 Title: file.title,
@@ -74,7 +74,7 @@ export class ClearUnusedFilesCommand {
 
         if (!isDryRun) {
             console.log('Удаление файлов...');
-            await Promise.all(junkFilesIds.map(fileId => this.fileService.remove(fileId, null)));
+            await Promise.all(unusedFilesIds.map(fileId => this.fileService.remove(fileId, null)));
             console.log('Файлы удалены!');
         } else {
             console.log('Установлен флаг dry run, поэтому удаления файлов не произошло');
