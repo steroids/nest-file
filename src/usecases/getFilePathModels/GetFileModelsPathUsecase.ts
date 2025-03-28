6e74441d0cb68e57c9997d57e022421687838257
Danil <danil_ustinov@mail.ru>
2025-03-28 16:26:29 +0700

    fix: rebasing

--- src/usecases/getFilePathModels/GetFileModelsPathUsecase.ts
+++ src/usecases/getFilePathModels/GetFileModelsPathUsecase.ts
@@ -1,6 +1,6 @@
 import {FileService} from '../../domain/services/FileService';
 import {FileImageService} from '../../domain/services/FileImageService';
-import {FileStorage} from '../../domain/enums/FileStorageEnum';
+import {FileStorageEnum} from '../../domain/enums/FileStorageEnum';
 
 export class GetFileModelsPathUsecase {
     constructor(
@@ -8,7 +8,7 @@ export class GetFileModelsPathUsecase {
         protected readonly fileImageService: FileImageService,
     ) {}
 
-    async handle(storageName: FileStorage) {
+    async handle(storageName: FileStorageEnum) {
         return [
             ...await this.fileImageService.getFilesPathsFromDb(storageName),
             ...await this.fileService.getFilesPathsFromDb(storageName),
