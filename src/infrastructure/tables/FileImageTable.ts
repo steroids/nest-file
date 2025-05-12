8e3df6e9c2ba0596c915854707d3ba21540dd7e9
Vladislav Koktashev <vladislav54415@gmail.com>
2025-05-12 17:20:21 +0300

    0.3.0 version update

--- src/infrastructure/tables/FileImageTable.ts
+++ src/infrastructure/tables/FileImageTable.ts
@@ -1,6 +1,6 @@
-import {TableFromModel} from '@steroidsjs/nest/infrastructure/decorators/TableFromModel';
 import {IDeepPartial} from '@steroidsjs/nest/usecases/interfaces/IDeepPartial';
-import { FileImageModel } from '../../domain/models/FileImageModel';
+import {TypeOrmTableFromModel} from '@steroidsjs/nest/infrastructure/decorators/typeorm/TypeOrmTableFromModel';
+import {FileImageModel} from '../../domain/models/FileImageModel';
 
-@TableFromModel(FileImageModel, 'file_image')
+@TypeOrmTableFromModel(FileImageModel, 'file_image')
 export class FileImageTable implements IDeepPartial<FileImageModel> {}
