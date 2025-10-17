63ddafc0c4eb7c79b0ee2af6a7cc15b8fcb5bc8b
Vladislav Koktashev <vladislav54415@gmail.com>
2025-10-17 12:28:44 +0300

    docs

--- src/usecases/getFileStorageParams/interfaces/IGetFileStorageParamsUseCase.ts
+++ src/usecases/getFileStorageParams/interfaces/IGetFileStorageParamsUseCase.ts
@@ -1,5 +1,8 @@
 export const GET_FILE_STORAGE_PARAMS_USE_CASE_TOKEN = 'get_file_storage_params_use_case_token';
 
+/*
+    Позволяет задать параметры загрузки файла в определенный тип хранилища
+ */
 export interface IGetFileStorageParamsUseCase {
-    handle: (fileType: string) => Promise<Record<string, any>>,
+    handle: (fileType: string, storageName: string) => Promise<Record<string, any>>,
 }
